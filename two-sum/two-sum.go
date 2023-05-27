687177ffb070fa2bf4b3ea9f7cbff16c428da9af
Caesario Dito <caesariodito2014@gmail.com>
2023-05-27 23:54:18 +0700

    move code
    merge optimized code and my solution to 1 file

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -1,10 +1,23 @@
+// personal
 func twoSum(nums []int, target int) []int {
-    numToIndex := make(map[int]int)
-    for i := 0; i < len(nums); i++ {
-        if j, ok := numToIndex[target-nums[i]]; ok {
-            return []int{j, i}
-        }
-        numToIndex[nums[i]] = i
-    }
-    return []int{}
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if nums[i]+nums[j] == target && i != j {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+// optimized, convert from python
+func twoSum(nums []int, target int) []int {
+	numToIndex := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if j, ok := numToIndex[target-nums[i]]; ok {
+			return []int{j, i}
+		}
+		numToIndex[nums[i]] = i
+	}
+	return []int{}
 }
--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,6 +1,16 @@
 class Solution:
+    # personal
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if nums[i] + nums[j] == target and i != j:
                     return [i, j]
+    
+    # optimized, got from other source
+    def twoSumOpt(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        return []
