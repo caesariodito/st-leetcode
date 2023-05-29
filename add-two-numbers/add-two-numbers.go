3ac41eb91559e8ee717be75d1e1845711505c6f4
Caesario Dito <caesariodito@gmail.com>
2023-05-30 01:35:36 +0700

    Time: 11 ms (58.62%) | Memory: 4.5 MB (82.68%) - LeetSync

--- add-two-numbers/add-two-numbers.go
+++ add-two-numbers/add-two-numbers.go
@@ -32,3 +32,11 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
     return dummyHead.Next
 }
+
+func reverseString(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
