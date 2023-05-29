bcdfa83ea651ce7688f5aab41b6ca76138c5ba66
Caesario Dito <caesariodito@gmail.com>
2023-05-30 01:34:35 +0700

    Time: 11 ms (58.62%) | Memory: 4.5 MB (82.68%) - LeetSync

---
+++ add-two-numbers/add-two-numbers.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    dummyHead := &ListNode{Val: 0}
+    currentNode := dummyHead
+    carry := 0
+
+    for l1 != nil || l2 != nil {
+        var x, y int
+        if l1 != nil {
+            x = l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            y = l2.Val
+            l2 = l2.Next
+        }
+        sum := carry + x + y
+        carry = sum / 10
+        currentNode.Next = &ListNode{Val: sum % 10}
+        currentNode = currentNode.Next
+    }
+
+    if carry > 0 {
+        currentNode.Next = &ListNode{Val: carry}
+    }
+
+    return dummyHead.Next
+}
+
+func reverseString(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
